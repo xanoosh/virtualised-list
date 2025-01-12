a75484fc39eb55eef1a60a4c79c475d62165f20c
xanoosh <xanoosh@gmail.com>
2025-01-12 15:15:42 +0100

    list element component, styling

--- src/components/VirtualisedList.tsx
+++ src/components/VirtualisedList.tsx
@@ -1,6 +1,7 @@
 import { useRef } from 'react';
 import { useVirtualizer } from '@tanstack/react-virtual';
 import { useListData } from '../hooks/useListData';
+import VirtualisedListElement from './VirtualisedListElement';
 
 export default function VirtualisedList() {
   const parentRef = useRef<HTMLDivElement>(null);
@@ -12,7 +13,7 @@ export default function VirtualisedList() {
   const virtualizer = useVirtualizer({
     count: listItems?.length || 0,
     getScrollElement: () => parentRef.current,
-    estimateSize: () => 70,
+    estimateSize: () => 120,
   });
 
   const virtualItems = virtualizer.getVirtualItems();
@@ -32,31 +33,11 @@ export default function VirtualisedList() {
           {virtualItems.map((virtualItem) => {
             const listItem = listItems[virtualItem.index];
             return (
-              <li
+              <VirtualisedListElement
                 key={virtualItem.key}
-                className="absolute top-0 left-0 w-full flex justify-start items-center py-2 px-4"
-                style={{
-                  height: `${virtualItem.size}px`,
-                  transform: `translateY(${virtualItem.start}px)`,
-                }}
-              >
-                <blockquote className="italic font-semibold text-white">
-                  <svg
-                    className="w-4 h-4 text-slate-300 mb-4"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 18 14"
-                  >
-                    <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
-                  </svg>
-                  <p>{listItem?.quote}</p>
-                </blockquote>
-
-                <small className="absolute bottom-0 right-0 text-slate-300 text-sm">
-                  {listItem?.author}
-                </small>
-              </li>
+                virtualItem={virtualItem}
+                listItem={listItem}
+              />
             );
           })}
         </ul>
---
+++ src/components/VirtualisedListElement.tsx
@@ -0,0 +1,34 @@
+export default function VirtualisedListElement({
+  virtualItem,
+  listItem,
+}: {
+  virtualItem: { size: number; start: number };
+  listItem: { quote: string; author: string };
+}) {
+  return (
+    <li
+      className="absolute top-0 left-0 w-full flex justify-start items-center p-4"
+      style={{
+        height: `${virtualItem.size}px`,
+        transform: `translateY(${virtualItem.start}px)`,
+      }}
+    >
+      <blockquote className="text-white text-left">
+        <svg
+          className="w-4 h-4 text-slate-300 mb-2"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 18 14"
+        >
+          <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
+        </svg>
+        <i className="">"{listItem?.quote}"</i>
+      </blockquote>
+
+      <small className="absolute bottom-2 right-4 text-slate-300 text-xs">
+        {listItem?.author}
+      </small>
+    </li>
+  );
+}
