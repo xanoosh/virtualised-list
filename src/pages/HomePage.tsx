4a9ac7cc0fbdbe36c9921f4a3ff247678015e122
xanoosh <xanoosh@gmail.com>
2025-01-27 11:29:38 +0100

    added description

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,10 +3,11 @@ export default function HomePage() {
     <div className="text-center flex flex-col gap-4">
       <h1 className="text-4xl font-semibold">Home page</h1>
       <p className="text-lg">
-        Simple app showcasing listvirtualisation as well as lazy loading. Both
-        examples are built with custom hooks leveraging tanstack query
-        <b>(useInfiniteQuery, useQuery)</b> and data consumption from 2 simple
-        APIs. Deployed on Vercel'
+        Simple app showcasing list virtualisation as well as lazy loading
+        content. Both examples are built with custom hooks leveraging tanstack
+        query
+        <b>(useQuery + useVirtualizer, useInfiniteQuery)</b> and data
+        consumption from 2 simple APIs. Deployed on Vercel
       </p>
     </div>
   );
